35b01843c7ee90f4b4c1b7900d0f92cf10a2bb50
gauravgaria <gaurav.garia93@gmail.com>
2026-06-03 09:17:22 +0530

    problem 2

--- src/arrays/DuplicateNumber.java
+++ src/arrays/DuplicateNumber.java
@@ -5,8 +5,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DuplicateNumber {
+    // TC: O(n) and SC: O(n)
+    // So If there is optimization on memory or memory constraint go for sorting approach
     public static boolean hasDuplicate(int[] nums){
-        if(nums == null || nums.length < 1){
+        if(nums == null || nums.length == 0){
             return false;
         }
         Set<Integer> seen = new HashSet<Integer>();
@@ -18,8 +20,22 @@ public class DuplicateNumber {
         }
         return false;
     }
+    // TC: n log n and SC: O(1)
+    public static boolean hasDuplicateUsingSorting(int[] nums){
+        if(nums == null || nums.length == 0){
+            return false;
+        }
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        int[] nums = new int[]{1,4,11,2};
+        int[] nums = new int[]{1,4,1,2};
         System.out.println(hasDuplicate(nums));
+        System.out.println(hasDuplicateUsingSorting(nums));
     }
 }
